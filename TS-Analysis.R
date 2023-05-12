869e577af20cb14fa7dc8d21f9254fa96b242d08
jjfrackowiak <froncek1231@gmail.com>
2023-05-12 12:06:08 +0200

    Very primitive ver. of our app

--- TS-Analysis.R
+++ TS-Analysis.R
@@ -1,7 +1,8 @@
 library(shiny)
+library(forecast)
 
 ui <- fluidPage(
-  titlePanel("Prediction of Interest Rate Value r - ARIMA Model"),
+  titlePanel("Prediction of Interest Rate Value r - ARIMAX Model"),
   
   sidebarLayout(
     sidebarPanel(
@@ -11,11 +12,11 @@ ui <- fluidPage(
       radioButtons("sep", "Separator",
                    choices = c(Comma = ",", Semicolon = ";", Tab = "\t"),
                    selected = ","),
-      numericInput("number3",
+      numericInput("number",
                    label = "Enter the value of x for which you want to predict r. The predicted value of the interest rate for the selected x value will be displayed below the graph",
                    value = ""),
       br(),
-      checkboxInput("line", "Display the line obtained by regression?", value = TRUE)
+      checkboxInput("line", "Display the fitted line?", value = TRUE)
     ),
     
     mainPanel(
@@ -37,15 +38,28 @@ server <- function(input, output) {
   output$plot1 <- renderPlot({
     plot(data()$x, data()$r,
          xlim = c(0, 10),
-         ylim = c(0, 10))
+         ylim = c(0, 10), col = "blue")
     if(input$line) {
-      model <- forecast::Arima(data()$r, order = c(1, 0, 0), xreg = data()$x)
-      abline(coef(model))
+      lm_fit <- lm(r ~ x, data = data())
+      abline(lm_fit)
+      arima_fit <- arima(data()$r, order = c(1, 0, 1), xreg = data()$x)
+      lines(data()$x, arima_fit$fitted, col = "red")
+    
+      legend("bottomright", legend = c("r", "ARIMAX Fit"), 
+             col = c("blue","red"), lty = 1)
     }
   })
   
   output$selected_x <- renderText({
-    model <- forecast::Arima(data()$r, order = c(1, 0, 0), xreg = data()$x)
-    forecast_result <- forecast::forecast.Arima(model, h = 1, xreg = data.frame(x = input$number3))
-    pred_value <- forecast_result$mean[1]
-    paste("Predicted
+    arima_fit <- arima(data()$r, order = c(1,1,0), xreg = data()$x)
+    pred_value <- predict(arima_fit, n.ahead = 1, newxreg = data.frame(x = as.numeric(input$number)))
+    print(length(arima_fit$fitted))
+    paste("Predicted value of the interest rate r for the selected x value: ", pred_value$pred)
+  })
+  
+  
+}
+
+
+shinyApp(ui = ui, server = server)
+
